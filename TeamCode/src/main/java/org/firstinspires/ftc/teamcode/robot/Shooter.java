9945435d2e3cc37a953f91dd4dbb3869064bc3b2
Joshua Vigel <joshvigel329@outlook.com>
2020-10-29 19:46:11 -0400

    WIP : Added shooter math

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.robot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
@@ -35,10 +34,45 @@ public class Shooter {
             shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             shooter.setMode(RUN_USING_ENCODER);
         }
-
         return success;
     }
 
+    private double calculate(double distance, double hight, double hightOfShooter){
+        double peakY = hight;
+        double shooterHight =  hightOfShooter;
+        //hight
+        double h = peakY - shooterHight;
+        double dis = distance;
+
+        double g = 9.81 *3.28084 *12;
+        //diameter of the wheels
+        double dia = 4;
+        //cicumpherence of the wheels
+        double cir = dia * Math.PI;
+        //starting y velocity
+        double Vynot = Math.sqrt(2 * h * g);
+        //starting x velocity
+        double Vxnot = ((dis * g) / Vynot);
+        double atanconst = Vynot/Vxnot;
+        double theta = Math.atan(atanconst);
+        double Vnot = Math.sqrt(Math.pow(Vxnot, 2) + Math.pow(Vynot, 2));
+        double rpm = 2 * (Vnot / cir) * 60;
+
+        double t = Vynot/g + Math.sqrt(Math.pow(Vynot, 2)/Math.pow(g, 2) - 2 * (-hightOfShooter)/g);
+        double xDist = Vxnot * t;
+
+        return rpm;
+    }
+    //Josh was here
+    public void shoot()
+    {
+        shooter.setPower(calculate(16, 36, 12));
+    }
+
+    private final double SHOOTER_CPER = 6000; //quad encoder cnts/encoder rev
+    private final double SHOOTER_INT_GEAR = 1; //Neverest 20
+    private final double SHOOTER_EXT_GEAR = 1.0;
+    private final double LIFTER_CPR = SHOOTER_CPER * SHOOTER_INT_GEAR * SHOOTER_EXT_GEAR;
     protected HardwareMap hwMap;
     public DcMotorEx shooter;
     private static final String TAG = "SJH_SHT";
