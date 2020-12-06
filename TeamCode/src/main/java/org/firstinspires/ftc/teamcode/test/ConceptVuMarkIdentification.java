d6b11a07a353828b18afd24aeca76d85fdb23ec9
CoachH <shelbyrobotics@nope.com>
2020-12-06 14:12:50 -0500

    Disable some old test opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ConceptVuMarkIdentification.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ConceptVuMarkIdentification.java
@@ -31,6 +31,7 @@ package org.firstinspires.ftc.teamcode.test;
 import android.graphics.Bitmap;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.RobotLog;
@@ -72,7 +73,7 @@ import static org.firstinspires.ftc.teamcode.test.ConceptVuMarkIdentification.Je
 
 @SuppressWarnings({"unused", "DanglingJavadoc", "JavaDoc", "WeakerAccess", "ConstantConditions", "FieldCanBeLocal", "RedundantArrayCreation"})
 @Autonomous(name="Concept: VuMark Id - OpenCV", group ="Concept")
-//@Disabled
+@Disabled
 public class ConceptVuMarkIdentification extends LinearOpMode {
 
     public static final String TAG = "Vuforia VuMark Sample";
