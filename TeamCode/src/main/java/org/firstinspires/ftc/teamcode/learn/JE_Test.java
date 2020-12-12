1b3cfcbf0934e7651edfde6d50aceffa2e8cef58
Joshua Epee <joshuaepee1@gmail.com>
2020-12-12 13:24:24 -0500

    Added JE tesbot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/learn/JE_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/learn/JE_Test.java
@@ -61,7 +61,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
+@Autonomous(name="JE_TEST", group="Pushbot")
 @Disabled
 public class JE_Test extends  LinearOpMode{
     /* Declare OpMode members. */
