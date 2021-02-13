fd8aca7aa1e26f5769ccfc4ac9331acd55b173a1
CoachH <shelbyrobotics@nope.com>
2021-02-12 20:59:30 -0500

    Added CamMount class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/CamMount.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.RobotLog;
+
+public class CamMount
+{
+  public CamMount(HardwareMap map) { this.hwMap = map; }
+
+  public boolean init(){
+
+    boolean success;
+
+    try{
+      camServo = hwMap.get(Servo.class, "CamServo");
+      success = true;
+      setCamPos(RobotConstants.CamRot);
+    }catch (Exception e)
+    {
+      RobotLog.ee(TAG, "ERROR: Cam Servo missing");
+      success = false;
+    }
+    return success;
+  }
+
+  public void setCamPos(double pos){
+    camServo.setPosition(pos);
+  }
+
+  private static final String TAG = "SJH_CAM";
+  public Servo camServo;
+  protected HardwareMap hwMap;
+}
